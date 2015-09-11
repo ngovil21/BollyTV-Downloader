bc2a25b3ca90f204f1764291d96243999aba41ea
ngovil21 <ngovil21@gmail.com>
2015-09-11 16:17:23 -0500

    fix support for old path naming

--- Scraper/DesiTVBox.py
+++ Scraper/DesiTVBox.py
@@ -109,7 +109,7 @@ def Download(channel, shows, hd=False):
                     print(title)
                     path_old = os.path.join(path, show + " - " + date)
                     path = os.path.join(path, title)
-                    if os.path.exists(path) or os.path.exists(path_old):
+                    if os.path.exists(path):
                         if len(os.listdir(path)) > 0:
                             print("Non-empty folder exists. Assume already downloaded")
                             continue
