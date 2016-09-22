e1ede52891a1a3cdce396f498148ebcff306d486
ngovil21 <ngovil21@gmail.com>
2016-09-21 19:05:37 -0500

    dowload mp4 from playu

--- BollyTV/Hosters/Playu.py
+++ BollyTV/Hosters/Playu.py
@@ -8,8 +8,8 @@ import BollyTV.Common
 def get_download_link(url):
     source = BollyTV.Common.read_url(url)
     source = BollyTV.Common.replace_special_characters(source)
-    file = re.compile('\{file:[ ]*?"(http:[^.mp4"]+)"').findall(source)
-    if file:
-        return file[0] + ".mp4"
-    else:
-        return None
+    files = re.compile('\{file:[ ]*?"(http:[^"]+)"').findall(source)
+    for file in files:
+        if file.endswith("mp4"):
+            return file
+    return None
